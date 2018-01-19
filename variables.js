f0a4ba728a9e3ac42604c7276984019a718bdc78
Shaun Tung <shaun.tung@gmail.com>
2018-01-19 07:10:21 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -12,5 +12,10 @@ console.log(personality);
 // change the value of the `personality` variable to something that describes you
 // and console.log the new value
 
+<<<<<<< HEAD
 var personality = "funny"
 console.log(personality)
+=======
+personality = "funny";
+console.log(personality);
+>>>>>>> de3e95e6149696b85936bee00d5ef6aa89d0096f
